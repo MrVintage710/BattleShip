fc81975abe68380a5f5d19623192be3e8577522e
BuildTools <unconfigured@null.spigotmc.org>
2019-09-22 22:12:48 -0600

    Added some socket

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ class Room {
         this.uuid = uuid();
         this.player1 = player1;
         this.player2 = player2;
+        this.player1_board = null;
+        this.player2_board = null;
 
         player1.join(this.uuid);
         player2.join(this.uuid);
@@ -64,16 +66,32 @@ class Room {
 
         for( var i = 0; i < rooms.length; i++){
             if ( rooms[i].uuid == this.uuid) {
-              rooms.splice(i, 1);
-              console.log("Removed room from rooms.")
+                rooms.splice(i, 1);
+                console.log("Removed room from rooms.")
             }
-         }
+        }
 
         console.log(`Room(${this.uuid}) closed because Player(${player.uuid}) left.`)
     }
 
     broadcast(name, message) {
         io.to(this.uuid).emit(name, message);
+    }
+
+    saveBoard(player, board) {
+        if(player.uuid == this.player1.uuid) {
+            this.player1_board = board;
+        } else if(player.uuid == this.player2.uuid) {
+            this.player2_board = board;
+        }
+
+        if(this.player1_board != null && this.player2_board != null) {
+            if(Math.random >= 0.51) {
+                this.player1.emit("your_turn", {})
+            } else {
+                this.player2.emit("your_turn", {})
+            }
+        }
     }
 }
 
@@ -100,6 +118,16 @@ io.on('connection', (socket) => {
 
         console.log(`Player left: ${socket.uuid}`)
     })
+
+    socket.on('fire', function(msg){
+        console.log(`Player ${socket.uuid} fired at ${msg.x}, ${msg.y}` )
+    });
+
+    socket.on('board_finished', function(msg){
+        socket.room.saveBoard(socket, msg.localBoard)
+
+        console.log(socket.room.player1_board)
+    });
 });
 
 console.log("The port passed: " + parseInt(process.argv[2]));
