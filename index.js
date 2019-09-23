b1eeae1746906374ed42e23b483806c7a01278b9
BuildTools <unconfigured@null.spigotmc.org>
2019-09-22 23:29:06 -0600

    adjfkl;asjdf

--- index.js
+++ index.js
@@ -49,6 +49,18 @@ class Room {
         player1.join(this.uuid);
         player2.join(this.uuid);
 
+        this.player1_carrier = 5;
+        this.player1_battleship = 4;
+        this.player1_submarine = 3;
+        this.player1_cruiser = 3;
+        this.player1_destroyer = 2;
+
+        this.player2_carrier = 5;
+        this.player2_battleship = 4;
+        this.player2_submarine = 3;
+        this.player2_cruiser = 3;
+        this.player2_destroyer = 2;
+
         player1.room = this;
         player2.room = this;
 
@@ -59,9 +71,13 @@ class Room {
         if(this.player1 = player) {
             this.player2.room = null;
             queue.push(this.player2);
+
+            this.player2.emit("back_to_lobby", {})
         } else {
             this.player1.room = null;
             queue.push(this.player1);
+
+            this.player1.emit("back_to_lobby", {})
         }
 
         for( var i = 0; i < rooms.length; i++){
@@ -93,6 +109,50 @@ class Room {
             }
         }
     }
+
+    getBoard(player) {
+        if(player.uuid == this.player1.uuid) {
+            return this.player1_board;
+        } else if(player.uuid == this.player2.uuid) {
+            return this.player2_board;
+        }
+    }
+
+    getOtherPlayer(player) {
+        if(player.uuid == this.player1.uuid) {
+            return this.player2;
+        } else if(player.uuid == this.player2.uuid) {
+            return this.player1;
+        }
+    }
+
+    getPlayerInfo(player) {
+        if(player.uuid == this.player1.uuid) {
+            return {"C":this.player1_carrier, 
+                    "B": this.player1_battleship,
+                    "S": this.player1_submarine,
+                    "R": this,player1_cruiser,
+                    "D": this.player1_destroyer};
+        } else if(player.uuid == this.player2.uuid) {
+            return {"C":this.player2_carrier, 
+                    "B": this.player2_battleship,
+                    "S": this.player2_submarine,
+                    "R": this,player2_cruiser,
+                    "D": this.player2_destroyer};
+        }
+    }
+
+    hitPlayer(player, type) {
+        if(player.uuid == this.player1.uuid) {
+            switch(type) {
+                case "C":
+                    this.player1_carrier--;
+                    return this.player1_carrier;
+            }
+        } else if(player.uuid == this.player2.uuid) {
+            return this.player1;
+        }
+    }
 }
 
 var queue = new Queue();
@@ -121,6 +181,25 @@ io.on('connection', (socket) => {
 
     socket.on('fire', function(msg){
         console.log(`Player ${socket.uuid} fired at ${msg.x}, ${msg.y}` )
+
+        var otherPlayer = socket.room.getOtherPlayer(socket);
+        var board  = socket.room.getBoard(otherPlayer);
+
+        var value = board[msg.x-1][msg.y-1]
+
+        if(value != "_") {
+           var result = socket.room.hitPlayer(otherPlayer, value)
+           if(result <= 0) {
+                otherPlayer.emit("sunk", {"type":value, "coord":`${msg.x}:${msg.y}`})
+                socket.emit("sunk", {"type":value, "coord":`e${msg.x}:${msg.y}`})
+           } else {
+                otherPlayer.emit("hit", {"coord":`${msg.x}:${msg.y}`})
+                socket.emit("hit", {"coord":`e${msg.x}:${msg.y}`})
+           }
+        } else {
+            otherPlayer.emit("missed", {"coord":`${msg.x}:${msg.y}`})
+            socket.emit("missed", {"coord":`e${msg.x}:${msg.y}`})
+        }
     });
 
     socket.on('board_finished', function(msg){
